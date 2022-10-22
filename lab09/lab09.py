6e6ec8010930391de87f222af03d29a2a42cef9d
Doug Van Horn <dvanhorn@outsideinc.com>
2022-10-22 06:43:58 -0500

    Implement selection sort as Ethan did.

--- lab09/lab09.py
+++ lab09/lab09.py
@@ -87,6 +87,42 @@ def main():
     for record in records:
         print(record)
 
+    selection_sort_by_fine(records)
+    print()
+    print('By manual sort fine')
+    print('-' * 79)
+    for record in records:
+        print(record)
+
+
+def selection_sort_by_fine(records):
+    # Algorithm:
+    # https://en.wikipedia.org/wiki/Selection_sort
+    # Given list of size n.
+    # Start at the first element, i.
+    # Traverse i+1..n, to get the index of the largest element largest_i
+    # Swap the largest element for the first postion.
+    print()
+    record_count = len(records)
+
+    # Outer loop goes from 0 to n-1.
+    for left_index in range(0, record_count - 1):
+        print(' '.join([str(record.get_fine()) for record in records]))
+        # Left index?
+        index_of_greatest_fine = left_index
+
+        # Inner loop 
+        for comparing_index in range(left_index+1, record_count):
+            print(f'    {records[comparing_index].get_fine()} > {records[index_of_greatest_fine].get_fine()}?')
+            if records[comparing_index].get_fine() > records[index_of_greatest_fine].get_fine():
+                print(f'        moving index_of_greatest_fine from {index_of_greatest_fine} to {comparing_index}')
+                index_of_greatest_fine = comparing_index
+        
+        print(f'left_index[{left_index}]: {records[left_index].get_fine()}')
+        print(f'index_of_greatest_fine[{index_of_greatest_fine}]: {records[index_of_greatest_fine].get_fine()}')
+        print()
+        records[left_index], records[index_of_greatest_fine] = records[index_of_greatest_fine], records[left_index]
+
 
 if __name__ == '__main__':
     main()
